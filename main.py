9ae099ba230d4771a350443e6557f8beb38fb1b6
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2023-04-08 18:27:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if latest_version != current_version:
 
     }
     response = requests.post(telegram_api_url, data=params)
-    print(response1.status_code)
+    print(response.status_code)
     with open('.env', 'w') as f:
         f.write(f"version={latest_version}")
     with open('.env', 'r') as f:
