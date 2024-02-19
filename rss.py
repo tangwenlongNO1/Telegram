2105ac64ba260d0b08373ea3c1327a7b5a39de57
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2024-02-19 16:47:57 +0800

    Update rss.py

--- rss.py
+++ rss.py
@@ -15,7 +15,7 @@ rss_feed_url2 = 'https://rsshub.app/asiantolick/tag/2256'
 def fetch_latest_items():
     feed = feedparser.parse(rss_feed_url2)
     latest_entry = feed.entries[0]
-    soup = BeautifulSoup(latest_entry.description, 'html.parser')
+    soup = BeautifulSoup(feed.description, 'html.parser')
     first_image = soup.find_all('img')
 
     
