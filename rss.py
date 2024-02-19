d72027269e57ad13136ccde80028895c326146f6
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2024-02-19 14:50:39 +0800

    Update rss.py

--- rss.py
+++ rss.py
@@ -9,21 +9,24 @@ telegram_chat_id = os.environ.get('TG_CHAT_ID')
 # RSS订阅的URL
 rss_feed_url = 'https://www.141jav.com/feeds/'
 rss_feed_url1 = 'https://rsshub.app/1x/latest'
-rss_feed_url2 = 'https://rsshub.app/tits-guru/home'
+rss_feed_url2 = 'https://rsshub.app/95mm/tag/%E9%BB%91%E4%B8%9D'
 
 def fetch_latest_items():
     feed = feedparser.parse(rss_feed_url2)
     latest_entry = feed.entries[0]
-    # soup = BeautifulSoup(latest_entry.description, 'html.parser')
-    # first_image = soup.find('img')
+    soup = BeautifulSoup(latest_entry.description, 'html.parser')
+    first_image = soup.find('img')
+
     
-    # # 获取第一张图片链接
-    # if first_image:
-    #     first_image_url = first_image['src']
-    #     return f"{first_image_url}"
-    # else:
-    #     return None
-    return f"{latest_entry.description[10:-4]}"
+    
+    # 获取第一张图片链接
+    if first_image:
+        first_image_url = first_image['src']
+        print(first_image_url)
+        return f"{first_image_url}"
+    else:
+         return None
+    # return f"{latest_entry.description[10:-4]}"
 
 def send_message(message):
     url = f'https://api.telegram.org/bot{telegram_bot_token}/sendPhoto'
