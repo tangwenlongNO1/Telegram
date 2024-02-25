e260b77deb9ea1eaef9fa080367f281e44eb519c
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2024-02-25 12:40:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ repo = g.get_user(username).get_repo(repo_name)
 # 获取当前最新版本的 Clash For Windows Chinese_patch 下载链接和更新日志
 response = requests.get("https://api.github.com/repos/clash-verge-rev/clash-verge-rev/releases/latest")
 print(response.status_code)
-data = response.json(encoding='utf-8')
+data = response.json()
 latest_version = data['tag_name']
 latest_download_url = data['assets'][1]['browser_download_url']
 print(latest_download_url)
-# latest_changelog = data['body']
-latest_changelog = data.get('body')
-print(latest_changelog)
+latest_changelog = data['body']
+# latest_changelog = data.get('body')
+# print(latest_changelog)
 re = requests.get("https://api.github.com/repos/tangwenlongNO1/Telegram/contents/.env")
 dt = re.json()
 sha = dt['sha']
