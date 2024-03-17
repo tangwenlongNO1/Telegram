3d7c6ba1da04af4f780e2fd5bca824250225dbbe
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2024-03-17 15:16:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ repo = g.get_user(username).get_repo(repo_name)
 response = requests.get("https://api.github.com/repos/clash-verge-rev/clash-verge-rev/releases/latest")
 print(response.status_code)
 data = response.json()
-# print(data)
+print(data['body'])
 latest_version = data['tag_name']
 latest_download_url = data['assets'][1]['browser_download_url']
 print(latest_download_url)
@@ -25,24 +25,24 @@ dt = re.json()
 sha = dt['sha']
 
 # 推送更新通知到 Telegram
-telegram_bot_token = os.environ.get('TG_TOKEN')
-telegram_chat_id = os.environ.get('TG_CHAT_ID')
-current_version = os.getenv('version')
-telegram_api_url = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage"
-if latest_version != current_version:
+# telegram_bot_token = os.environ.get('TG_TOKEN')
+# telegram_chat_id = os.environ.get('TG_CHAT_ID')
+# current_version = os.getenv('version')
+# telegram_api_url = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage"
+# if latest_version != current_version:
     
-    message_text = f"🎉*Clash-verge-rev 更新至 {latest_version}*\n{latest_changelog}\n[下载链接](https://github.com/clash-verge-rev/clash-verge-rev/releases/tag/{latest_version})"
-    params = {
-        "chat_id":telegram_chat_id,
-        "text":message_text,
-        "parse_mode":'Markdown',
-        "disable_web_page_preview":False
+#     message_text = f"🎉*Clash-verge-rev 更新至 {latest_version}*\n{latest_changelog}\n[下载链接](https://github.com/clash-verge-rev/clash-verge-rev/releases/tag/{latest_version})"
+#     params = {
+#         "chat_id":telegram_chat_id,
+#         "text":message_text,
+#         "parse_mode":'Markdown',
+#         "disable_web_page_preview":False
 
-    }
-    response = requests.post(telegram_api_url, data=params)
-    print(response.status_code)
-    with open('.env', 'w') as f:
-        f.write(f"version={latest_version}")
-    with open('.env', 'r') as f:
-        contents = f.read()
-    repo.update_file(".env", "update .env", contents, sha, branch="master")
+#     }
+#     response = requests.post(telegram_api_url, data=params)
+#     print(response.status_code)
+#     with open('.env', 'w') as f:
+#         f.write(f"version={latest_version}")
+#     with open('.env', 'r') as f:
+#         contents = f.read()
+#     repo.update_file(".env", "update .env", contents, sha, branch="master")
