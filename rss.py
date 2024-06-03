f10c310a048e83c2816c4381a7fb5de4cb1e48c2
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2024-06-03 15:07:08 +0800

    Update rss.py

--- rss.py
+++ rss.py
@@ -14,6 +14,7 @@ rss_feed_url2 = 'https://rsshub.app/wallhaven/search/q=black+silk&categories=001
 
 def fetch_latest_items():
     feed = feedparser.parse(rss_feed_url2)
+    print(feed)
     latest_entry = feed.entries[random.randint(0, len(feed.entries) - 1)]
     soup = BeautifulSoup(latest_entry.description, 'html.parser')
     first_image = soup.find_all('img')
