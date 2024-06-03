5f9d679d6e4194cc95e8c1efc4b41f32a5e4a862
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2024-06-03 14:58:55 +0800

    Update 141.py

--- 141.py
+++ 141.py
@@ -15,7 +15,7 @@ rss_feed_url2 = "https://rsshub.app/2048/27"
 
 def fetch_latest_items():
     feed = feedparser.parse(rss_feed_url2)
-    latest_entry = feed.entries[random.randint(0, len(feed.entries) - 1)]
+    latest_entry = feed.entries[random.randint(0, 10)]
     soup = BeautifulSoup(latest_entry.description, 'html.parser')
     first_image = soup.find_all('img')
 
